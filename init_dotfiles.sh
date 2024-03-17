22fc96e4fa4879eda3d726bdf4f9f832c58fdbe6
Guo Zhi <qtxuning1999@sjtu.edu.cn>
2024-03-17 23:43:15 +0800

    refactor init dotfiles

--- init_dotfiles.sh
+++ init_dotfiles.sh
@@ -1,185 +1,181 @@
 #!/bin/bash
-guozhi_ipads_ip="192.168.25.3"
-localhost_ip="127.0.0.1"
-clash_port="7890"
+
+required_packages=(git vim zsh tmux node pip curl)
+all_dotfiles=("vpn" "git" "vim" "tmux" "zsh")
+selected_dotfiles=()
+use_vpn=false
 vpn_server=""
-
+is_company_machine=false
 hostname=$(uname -n)
+localhost_ip="127.0.0.1"
+clang_port="7890"
 
-vpn_selected=false
-git_selected=false
-vim_selected=false
-zsh_selected=false
-tmux_selected=false
-no_selected=true
-all_params=$@
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+BOLD='\033[1m'
+NONE='\033[0m'
 
-if [ ! "$HOME" == "$PWD" ]; then
-    printf "Incorrect usage, you should call me in home directory"
+cd "$(dirname "$0")"
+
+# check if the required packages are installed
+echo -e "Please make sure the following packages are installed:"
+echo -e "${BOLD}${required_packages[@]}${NONE}"
+for package in ${required_packages[@]}; do
+  if ! command -v $package &> /dev/null; then
+    printf "${RED}$package${NONE} is not installed, please install it first\n"
     exit
-fi
+  fi
+done
 
-if [ ! -d "dotfiles" ]; then
-    printf "Incorrect usage, dotfiles should be kept in home directory"    exit
-fi
+# Collect user input
+echo -e "Please input the dotfiles you want to update: vpn, git, vim, tmux, zsh, all"
+read all_params
 
-cd dotfiles
-
-
-if [[ "$all_params" == *"vpn"* ]]; then
-    vpn_selected=true
-    no_selected=false
-fi
-if [[ "$all_params" == *"git"* ]]; then
-    git_selected=true
-    no_selected=false
-fi
-if [[ "$all_params" == *"vim"* ]]; then
-    vim_selected=true
-    no_selected=false
-fi
-if [[ "$all_params" == *"zsh"* ]]; then
-    zsh_selected=true
-    no_selected=false
-fi
-if [[ "$all_params" == *"tmux"* ]]; then
-    tmux_selected=true
-    no_selected=false
-fi
-if [[ "$all_params" == *"all"* ]]; then
-    vpn_selected=true
-    git_selected=true
-    vim_selected=true
-    zsh_selected=true
-    tmux_selected=true
-    no_selected=false
+if [[ $all_params == "all" ]]; then
+  selected_dotfiles=("${all_dotfiles[@]}")
+else
+  for param in $all_params; do
+    # check param in all_dotfiles
+    if [[ " ${all_dotfiles[@]} " =~ " ${param} " ]]; then
+      selected_dotfiles+=($param)
+    fi
+  done
 fi
 
 if [[ $no_selected == true ]]; then
-    printf "Incorrect usage:\nInput vpn, git, vim, zsh, tmux for updating corresponding dotfiles.\nIpnut all for all selections\n"
-    exit
+  printf "No dotfiles selected, exit\n"
+  exit
 fi
+
+echo -e "${BLUE}Selected dotfiles: ${selected_dotfiles[@]}${NONE}"
+
+echo -e "Please input the vpn server ip and port, eg: 127.0.0.1:7890, if not use vpn, just press enter"
+read vpn_server
+# check vpn_server works or not
+export https_proxy=http://${vpn_server} http_proxy=http://${vpn_server} all_proxy=socks5://${vpn_server}
+if ! curl -sSf https://www.google.com > /dev/null; then
+  printf "${RED}Vpn server $vpn_server$ is not working, please check it${NONE}\n"
+  exit
+fi
+echo -e "${BLUE}Vpn server $vpn_server is working${NONE}"
+
+echo -e "Is this a company machine? (y/n)"
+read is_company_machine
+echo -e "${BLUE}$hostname is a company machine: $is_company_machine${NONE}"
 
 # vpn
-if [[ $vpn_selected == true ]]; then
-    arch=$(uname -i)
-    printf "init vpn on $hostname\n"
-    if [[ $arch == x86_64* ]]; then
-        printf "X64 Architecture\n"
-
-        cd clash
-        ./clash -d . &> /dev/null &
-
-        # run clash process after current shell exits
-        disown
-
-        vpn_server=${localhost_ip}:${clash_port}
-
-        cd ..
-    elif [[ $arch == arm* ]] || [[ $arch = aarch64 ]]; then
-        printf "ARM Architecutre\n"
-        vpn_server=${guozhi_ipads_ip}:${clash_port}
-    fi
-
-    # bootstrap proxy
-    export https_proxy=http://${vpn_server} http_proxy=http://${vpn_server} all_proxy=socks5://${vpn_server}
-    # bootstrap proxy
+if [[ " ${selected_dotfiles[@]} " =~ "vpn" ]]; then
+  arch=$(uname -i)
+  printf "Init ${BOLD}vpn${NONE} on $hostname\n"
+  if [[ $arch == x86_64* ]]; then
+    printf "  X64 Architecture\n"
+    ./clash/clash -d . &> /dev/null &
+    echo -e "  clash process is running on $clash_port"
+    # run clash process after current shell exits
+    disown
+    vpn_server=${localhost_ip}:${clash_port}
+  elif [[ $arch == arm* ]] || [[ $arch = aarch64 ]]; then
+    printf "  ARM Architecutre not supported yet\n"
+  fi
 fi
-#vpn
+# vpn
 
 # git
-if [[ $git_selected == true ]]; then
-    printf "init git config on $hostname\n"
-    cd git
+cd "$(dirname "$0")"
+if [[ " ${selected_dotfiles[@]} " =~ "git" ]]; then
+  printf "${BOLD}Init git config on $hostname${NONE}\n"
+  # we'd better set git config firstly, because git proxy can help us downlaod git project much more quickly
+  printf "backup origin gitconfig...\n"
+  if [ -f ~/.gitconfig]; then
+    mv ~/.gitconfig ./git/backup/gitconfig.`date +%F-%T`
+  fi
 
-    # first we should set git config correctly, because git proxy can help us downlaod git project much more quickly
-    printf "backup origin gitconfig...\n"
-    if [ -f "$HOME/.gitconfig" ]; then
-        mv ~/.gitconfig ./backup/gitconfig.`date +%F-%T`
-    fi
-
-    ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
-
-    cd ..
+  ln -s ./git/gitconfig ~/.gitconfig
 fi
 # git
 
 # vim
-if [[ $vim_selected == true ]]; then
-    printf "init vim on $hostname\n"
-    cd vim
+cd "$(dirname "$0")"
+if [[ " ${selected_dotfiles[@]} " =~ "vim" ]]; then
+  printf "${BOLD}Init vim on $hostname${NONE}\n"
+  cd vim
 
-    printf "backup origin vimrc...\n"
-    if [ -f "$HOME/.vimrc" ]; then
-        mv ~/.vimrc ./backup/vimrc.`date +%F-%T`
-    fi
+  printf "  Backup origin vimrc...\n"
+  if [ -f ~/.vimrc ]; then
+    mv ~/.vimrc ./backup/vimrc.`date +%F-%T`
+  fi
 
-    printf "create new vimrc...\n"
-    ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+  printf "  Create new vimrc...\n"
+  ln -s ./vimrc ~/.vimrc
 
-    git submodule init
-    git submodule update
+  if [[ $is_company_machine == true ]]; then
+    # remove wakatime plugin
+    sed -i '/wakatime/d' ./vimrc
+  fi
 
-    rm -rf ~/.vim
-    ln -s ~/dotfiles/vim ~/.vim
+  git submodule init
+  git submodule update
 
-    printf "Install vim plugins...\n"
-    bash ./init_vim.sh
+  rm -rf ~/.vim
+  ln -s ./vim ~/.vim
 
-    cd ..
+  printf "  Install vim plugins...\n"
+  bash ./init_vim.sh
 fi
 # vim
 
 # tmux
-if [[ $tmux_selected == true ]]; then
-    cd tmux
+cd "$(dirname "$0")"
+if [[ " ${selected_dotfiles[@]} " =~ "tmux" ]]; then
+  printf "${BOLD}Init tmux on $hostname${NONE}\n"
+  cd tmux
 
-    printf "backup origin tmux.conf...\n"
-    if [ -f "$HOME/.tmux.conf" ]; then
-        mv ~/.tmux.conf ./backup/tmux.conf.`date +%F-%T`
-    fi
+  printf "  Backup origin tmux.conf...\n"
+  if [ -f ~/.tmux.conf ]; then
+    mv ~/.tmux.conf ./backup/tmux.conf.`date +%F-%T`
+  fi
 
-    printf "create new tmux.conf...\n"
-    ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+  printf "  Create new tmux.conf...\n"
+  ln -s ./tmux/tmux.conf ~/.tmux.conf
+  tmux source ~/.tmux.conf
 
-    cd ..
+  printf "  Install tmux plugin framework: tpm\n"
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-    printf " Install tmux plugin framework: tpm\n"
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-    tmux source ~/.tmux.conf
-
-    # auto install tmux plugin
-    printf "Install tmux plugins\n"
-    bash ~/.tmux/plugins/tpm/scripts/install_plugins.sh
-    bash ~/.tmux/plugins/tpm/scripts/update_plugin.sh
+  # auto install tmux plugin
+  printf "  Install tmux plugins\n"
+  bash ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+  bash ~/.tmux/plugins/tpm/scripts/update_plugin.sh
 fi
 # tmux
 
 # zsh
-if [[ $zsh_selected == true ]]; then
-    cd zsh
+cd "$(dirname "$0")"
+if [[ " ${selected_dotfiles[@]} " =~ "zsh" ]]; then
+  printf "${BOLD}Init zsh on $hostname${NONE}\n"
+  cd zsh
 
-    printf "Backup origin zshrc...\n"
-    if [ -f "$HOME/.zshrc" ]; then
-        mv ~/.zshrc ./backup/zshrc.`date +%F-%T`
-    fi
+  printf "  Backup origin zshrc...\n"
+  if [ -f ~/.zshrc ]; then
+    mv ~/.zshrc ./backup/zshrc.`date +%F-%T`
+  fi
 
-    printf "Create new zshrc...\n"
-    ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+  printf "  Install oh-my-zsh...\n"
+  # bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  # Install oh-my-zsh by above command in scripts will lead scripts exit because of changing shell to zsh, ref to https://github.com/ohmyzsh/ohmyzsh/issues/5873
+  rm -rf ~/.oh-my-zsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-    # oh-my-zsh
-    # bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    # Install oh-my-zsh by above command in scripts will lead scripts exit because of changing shell to zsh, ref to https://github.com/ohmyzsh/ohmyzsh/issues/5873
-    printf "clone oh-my-zsh\n"
-    if [ ! -d "~/.oh-my-zsh" ]; then
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-    fi
+  printf "  Create new zshrc...\n"
+  rm -rf ~/.zshrc
+  ln -s ./zsh/zshrc ~/.zshrc
 
-    printf "Install zsh plugins...\n"
-    bash ./init_zsh.sh
+  printf "  Install zsh plugins...\n"
+  bash ./init_zsh.sh
 
-    chsh -s $(which zsh)
-    cd ..
+  chsh -s $(which zsh)
 fi
 # zsh
+
+cd "$(dirname "$0")"
+printf "${BLUE}Init dotfiles done!${NONE}\n"
--- vim/init_vim.sh
+++ vim/init_vim.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-printf "Install vim configurations...\n"
+printf "   Install vim configurations...\n"
 vim +PlugInstall +PlugUpdate +qall
-printf "Install coc plugins...\n"
+printf "   Install coc plugins...\n"
 vim +CocInstall coc-json coc-tsserver coc-pyright coc-clangd coc-snippets +qall
-printf "Eanble copilot...\n"
+printf "   Eanble copilot...\n"
 vim +Copilot setup +Copilot enable +qall
